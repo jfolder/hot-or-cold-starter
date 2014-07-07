7900cf6f95ceb2c972d8b3f20fa6f52b38f6a3d1
jfolder <jfolder@users.noreply.github.com>
2014-07-07 13:57:23 -0400

    First Website Commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,184 @@
-
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
+/********************** INITIALIZE TOP SCORE **************************/
+$("section").data("finalScore", 0);
+/**********************************************************************/
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
 
+
+/**************************** START: Functions *********************************/	
+function cleanUp() {
+	$("ul#guessList").empty();
+}
+function updateNumberOfGuesses() {
+	var count;
+	count = +$("span#count").text();
+	count++;
+	$("span#count").text(count);
+}
+function generateAnswer() {
+	return (1 + Math.floor(Math.random() * 100));
+}
+function getAnswer() {
+	return $("section>h3").data("answerStored");
+}
+function getGuess() {
+	var guess;
+	guess = +$("input#userGuess").val();
+	$("input#userGuess").val("");
+	if ((guess == NaN) || (guess%1 != 0) || (guess < 1) || (guess > 100) || !($.isNumeric(guess)) )
+	{
+		return "undefined";
+	}
+	else
+	{
+		$("ul#guessList").prepend("<li>" + guess + "</li>");
+		return guess;
+	}
+}
+function newGameConstructor(numberOfGuesses, score, feedback, answer) {
+	$("span#count").text(numberOfGuesses);
+	$("span#score").text(score);
+	$("span#score").data("scoreStored", score);
+	$("h2#feedback").text(feedback);
+	$("section>h3").data("answerStored", answer);
+}
+function newGame() {
+	var answer = generateAnswer();
+	var feedback = "Make Your Guess!";
+	var numberOfGuesses = 0;
+	var score = 10000; 
+	$("section > h3").empty().prepend("Currently have <span id=\"score\">0</span> points left!"); //reset text
+	$("input#userGuess").attr("disabled", false); //enable the input box
+	$("input#guessButton").attr("disabled", false); //enable the input button
+	cleanUp(); //remove left-over elements
+	newGameConstructor(numberOfGuesses, score, feedback, answer); //Initialize and construct our game
+}
+/***************************** END: Functions *********************************/
+
+
+
+
+
+/******************************** PLAY *****************************************/
+$("input#guessButton").click(function(e) {
+	var hunch;
+	var key;
+	var points;
+	var topScore;
+
+	hunch = getGuess();
+
+	if(key == "undefined") { 
+		e.preventDefault();
+	}
+	else
+	{
+		key = getAnswer();
+		
+		updateNumberOfGuesses();
+
+		points = +$("span#score").data("scoreStored");
+
+		//Check temperature and configure points
+		if (hunch == key) 
+		{
+			$("h2#feedback").text("Eureka! You found it!");
+			topScore = +$("section").data("finalScore");
+			$("section").data("finalScore", points);
+			$("input#userGuess").attr("disabled", true);
+			$("input#guessButton").attr("disabled", true);
+			if(points > topScore && topScore > 0) {
+				alert("Congratulations, you have beat your top score!!!");
+			}
+			else if(points == topScore) {
+				alert("Congratulations, you have matched your top score!");
+			}
+		}
+		else if (hunch >= (key - 10) && hunch <= (key + 10)) 
+		{
+			//100 points removed
+			$("h2#feedback").text("You are very hot");
+			points = points - 100;
+
+		}
+		else if (hunch >= (key - 20) && hunch <= (key + 20)) 
+		{
+			//200 points removed
+			$("h2#feedback").text("You are hot");
+			points = points - 200;
+		}
+		else if (hunch >= (key - 30) && hunch <= (key + 30)) 
+		{
+			//300 points removed
+			$("h2#feedback").text("You are warm");
+			points = points - 300;
+		}
+		else if (hunch >= (key - 50) && hunch <= (key + 50)) 
+		{
+			//500 points removed
+			$("h2#feedback").text("You are cold");
+			points = points - 500;
+		}
+		else {
+			//1000 points removed
+			$("h2#feedback").text("You are Ice Cold");
+			points = points - 1000;
+		}
+
+		//Display new text depending on score
+		if (points <= 3000)
+		{
+			$("section > h3").empty().prepend("A microscopic <span id=\"score\">0</span> points left!");
+		}
+		else if (points <= 5000)
+		{
+			$("section > h3").empty().prepend("Just another <span id=\"score\">0</span> points left!");
+		}
+		else if (points <= 7000) {
+			$("section > h3").empty().prepend("Only have <span id=\"score\">0</span> points left!");
+		}
+
+		//Display and store points
+		if (points < 1)	{
+			$("h2#feedback").text("GAME OVER");
+			$("section > h3").empty().prepend("You have no points left!");
+			$("span#score").text(0);
+			$("input#userGuess").attr("disabled", true);
+			$("input#guessButton").attr("disabled", true);
+		}
+		else {
+			$("span#score").text(points);
+			$("span#score").data("scoreStored", points);
+		}
+	}
+});
+/*******************************************************************************/
+
+
+
+
+/********************** BEGIN NEW GAME ******************************/
+newGame();
+jQuery(".new").click(function() {
+	newGame();
+});
+/******************************************************************/
+
+
+
+
+/*********************** DISPLAY/HIDE INFORMATION ******************************/
+
+/*--- Display information modal box ---*/
+$(".what").click(function(){
+	$(".overlay").fadeIn(1000);
 });
 
+/*--- Hide information modal box ---*/
+$("a.close").click(function(){
+	$(".overlay").fadeOut(1000);
+});
+/*******************************************************************************/
 
+});
